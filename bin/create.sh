c0c9dd163078e353ee652e7ce2c42d5f1f217a5f
sunnyD <simplydisconnected@reddit.com>
2023-03-22 20:53:27 -0500

    mount entire src directory

--- bin/create.sh
+++ bin/create.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash 
 
-exec docker create --volume "$(pwd)/etc":/RocksBot/etc --volume "$(pwd)/data":/RocksBot/data --name rocksbot rocksbot
+exec docker create --volume "$(pwd)":/RocksBot/ --name rocksbot rocksbot
